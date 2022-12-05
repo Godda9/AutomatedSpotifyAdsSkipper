0afe88eeadcabde1e8c49c9a43f7e67d9af6d535
Bohdan <bogdantelep@gmail.com>
2022-12-05 12:02:44 +0300

    Adding two more functional btns

--- AutomatedSpotifyAdsSkipper/AutomatedSpotifyAdsSkipper/AutomatedSpotifyAdsSkipper.cpp
+++ AutomatedSpotifyAdsSkipper/AutomatedSpotifyAdsSkipper/AutomatedSpotifyAdsSkipper.cpp
@@ -32,7 +32,7 @@ vector<DWORD> getProc() {
 }
 
 
-void SendCommand(DWORD dwProcessID) {
+void SendCommand(DWORD dwProcessID, int switch_arg) {
     HWND hwnd = NULL;
     do {
         hwnd = FindWindowEx(NULL, hwnd, NULL, NULL);
@@ -41,53 +41,75 @@ void SendCommand(DWORD dwProcessID) {
         if (dwPID == dwProcessID) {
             // cout << "[DESC]: " << hwnd << " [ID]: " << dwPID << '\n';
             HWND EditClass = FindWindowEx(hwnd, NULL, L"Chrome_RenderWidgetHostHWND", NULL);
-
-            PostMessage(EditClass, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_PLAY_PAUSE));
-            // PostMessage(EditClass, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_NEXTTRACK));
+            switch (switch_arg)
+            {
+                case 1:
+                    PostMessage(EditClass, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_PLAY_PAUSE));
+                    break;
+                case 2:
+                    PostMessage(EditClass, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_PREVIOUSTRACK));
+                    break;
+                case 3:
+                    PostMessage(EditClass, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_NEXTTRACK));
+                    break;
+            }
         }
     } while (hwnd != 0);
 }
 
 
 int main() {
-    string msg = "READY! Use R_SHIFT to skip. Do NOT close this window!";
+    string msg = "1) SHIFT + F1:\tPrevious track\n2) SHIFT + F2:\tNext track\n3) SHIFT + F3:\tSkip ads\n";
     string exec_spotify = "IF EXIST %appdata%\\Spotify\\Spotify.exe (start %appdata%\\Spotify\\Spotify.exe --minimized) ELSE (start spotify --minimized)";
     vector<DWORD> app_ids;
 
-    cout << msg << '\n';
+    cout << msg;
     while (true) {
-        if (GetAsyncKeyState(0xA1)) {
-            system("cls");
-            system("Color 7");
-            cout << "Processing Spotify";
-            system("taskkill /im Spotify.exe >nul 2>&1");
-            Sleep(1000);      
-            system(exec_spotify.c_str());
-
-
-            while (true) {
+        if (GetAsyncKeyState(0xA0)) {
+            if (GetAsyncKeyState(0x70)) {
                 app_ids = vector<DWORD>(getProc());
-                if (app_ids.size() > 4) {
-                    cout << '\n';
-                    Sleep(3500);
-                    for (auto& id : app_ids) {
-                        SendCommand(id);
-                    }
+                if (app_ids.size() > 4) for (auto& id : app_ids) SendCommand(id, 2);
+                Sleep(500);
+            }
 
-                    system("cls");
-                    system("Color A");
-                    for (int x = 3; x > 0; x--) {
-                        cout << "SUCCESS! This msg will disappear in "<< x << " seconds...";
-                        Sleep(1000);
+            if (GetAsyncKeyState(0x71)) {
+                app_ids = vector<DWORD>(getProc());
+                if (app_ids.size() > 4) for (auto& id : app_ids) SendCommand(id, 3);
+                Sleep(500);
+            }
+
+            if (GetAsyncKeyState(0x72)) {
+                system("cls");
+                system("Color 7");
+                cout << "Processing Spotify";
+                system("taskkill /im Spotify.exe >nul 2>&1");
+                Sleep(1000);
+                system(exec_spotify.c_str());
+
+                while (true) {
+                    app_ids = vector<DWORD>(getProc());
+                    if (app_ids.size() > 4) {
+                        cout << '\n';
+                        Sleep(3500);
+
+                        for (auto& id : app_ids) SendCommand(id, 1);
+
                         system("cls");
+                        system("Color A");
+
+                        for (int x = 3; x > 0; x--) {
+                            cout << "SUCCESS! This msg will disappear in " << x << " seconds...";
+                            Sleep(1000);
+                            system("cls");
+                        }
+
+                        system("Color 7");
+                        cout << msg;
+                        break;
                     }
-                    system("Color 7");
-                    cout << msg << '\n';
-                    break;
-                }
-                else {
-                    Sleep(50);
-                    cout << ".";
+                    else {
+                        Sleep(50); cout << ".";
+                    }
                 }
             }
         }
